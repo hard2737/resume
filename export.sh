da69c6659d6eae561b4f02d4ee606f2802ae9084
David Hardy <hard2737@gmail.com>
2024-07-26 14:32:49 -0600

    jabroni move, forgot contact info

---
+++ export.sh
@@ -0,0 +1,1 @@
+md-to-pdf README.md --document-title "Basic résumé of David Hardy" --pdf-options '{ "Author": "David Hardy", "format": "a4", "margin": { "top": "30mm", "right": "40mm", "bottom": "30mm", "left": "20mm" } }'  --config-file md-to-pdf.json
